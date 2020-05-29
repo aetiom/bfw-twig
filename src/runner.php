65c1882614d2e3b01b9b72dd5dedc2b1b0b60dee
aetiom <8208943+aetiom@users.noreply.github.com>
2020-05-29 08:41:36 +0200

    add twig debug extension
    add twig debug extension when debug option is set to true

--- src/runner.php
+++ src/runner.php
@@ -5,3 +5,7 @@ $config = $this->getConfig();
 $loader = new \Twig\Loader\FilesystemLoader($config->getValue('twigTemplates'));
 
 $this->twig = new \Twig\Environment($loader, $config->getValue('twigOptions'));
+
+if ($config->getValue('twigOptions')['debug'] === true) {
+    $this->twig->addExtension(new \Twig\Extension\DebugExtension());
+}
